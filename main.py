dbc430fe92cfb81453783c8df63a7fe35e23392d
Shlomi Badihi <shlomibadi100@gmail.com>
2022-01-14 12:40:30 +0200

    not sure if hats are inserted

--- main.py
+++ main.py
@@ -2,17 +2,43 @@ import sqlite3
 import atexit
 import sys
 import persistence
+import os
 
 
 def main():
-    config = sys.argv[1]
+    # read terminal arguments
+    config = open(sys.argv[1], "r")
     orders = sys.argv[2]
     output = sys.argv[3]
     database = sys.argv[4]
 
+    # figure out num of hats and num of suppliers
+    first_line_config = config.readline()
+    first_line_split = first_line_config.split(",")
+    num_of_hats = int(first_line_split[0])
+    num_of_suppliers = int(first_line_split[1][0:len(first_line_split[1])-1])
+
+    # create repository
     repo = persistence._Repository(database)
     atexit.register(repo._close)
     repo.create_tables()
+
+    # iterate over all hats
+    for i in range(0,num_of_hats):
+        # get hat variables
+        next_line = config.readline()
+        hat_variables = next_line.split(',')
+        hat_variables[3] = hat_variables[3][0:len(hat_variables[3])-1]
+
+        # create next hat
+        next_hat = persistence.Hat(int(hat_variables[0]), hat_variables[1], int(hat_variables[2]), int(hat_variables[3]))
+
+        repo.hats.insert(next_hat)
+
+
+
+
+
 
 
 
